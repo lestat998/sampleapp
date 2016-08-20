cd159b2571bea08419a0ec500e52817c4240b5b9
Javier Jimenez <lestat998@gmail.com>
2016-08-20 11:22:12 -0400

    setting smtp server

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,10 @@ class User < ApplicationRecord
 	has_secure_password
 	validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
-	def User.digest(string)
-		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+	  def User.digest(string)
+		  cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                       BCrypt::Engine.cost
-        BCrypt::Password.create(string, cost: cost)
+      BCrypt::Password.create(string, cost: cost)
     end
 
     def User.new_token
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,10 +29,7 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-  host = 'obscure-dawn-61289.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
+  
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
@@ -55,7 +52,10 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
-
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'obscure-dawn-61289.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "sample_app_#{Rails.env}"
