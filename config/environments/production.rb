c812d77dea677b603e5edcb2fac70f55990a5954
Javier Jimenez <lestat998@gmail.com>
2016-08-20 11:00:53 -0400

    setting smtp server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,9 +30,9 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
   config.action_mailer.raise_delivery_errors = true
-    config.action_mailer.delivery_method = :smtp
-    host = 'obscure-dawn-61289.herokuapp.com'
-    config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.delivery_method = :smtp
+  host = 'obscure-dawn-61289.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
